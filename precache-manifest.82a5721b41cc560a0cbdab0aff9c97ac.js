88a5330c49dc239be4f62d28b516684929227245
ericzijun <czij279@gmail.com>
2020-04-17 21:38:10 +1200

    Updates

---
+++ precache-manifest.82a5721b41cc560a0cbdab0aff9c97ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617a0a9f960c5887d543b674abca5703",
+    "url": "/portfolio_app/index.html"
+  },
+  {
+    "revision": "b42251b3e5eb5f346002",
+    "url": "/portfolio_app/static/css/main.24be3580.chunk.css"
+  },
+  {
+    "revision": "14724dce966190231075",
+    "url": "/portfolio_app/static/js/2.a59478a2.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portfolio_app/static/js/2.a59478a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42251b3e5eb5f346002",
+    "url": "/portfolio_app/static/js/main.41d4f33e.chunk.js"
+  },
+  {
+    "revision": "8b9b11e425af1960c856",
+    "url": "/portfolio_app/static/js/runtime-main.164cd7aa.js"
+  },
+  {
+    "revision": "32531585244869c103a1a3d2db259d38",
+    "url": "/portfolio_app/static/media/bjj1.32531585.jpg"
+  }
+]);
